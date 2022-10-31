3fcfb042f1a4bf43a007feff5f642ee7693ed1d2
harukiabe <hta12@scarletmail.rutgers.edu>
2022-10-31 02:48:08 -0400

    Initial Commit

---
+++ Personailty Quiz_HarukiAbe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personailty Quiz_HarukiAbe
+//
+//  Created by Haruki Abe on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
